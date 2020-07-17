d8e706a2b5e48f4f774420b58ca2fd1287cbbc16
Sidyvan Fernandes de Andrade <sidyvan.dev@gmail.com>
2020-07-16 22:15:03 -0300

    Removendo função em desuso

--- presencajacs/core/ajax.py
+++ presencajacs/core/ajax.py
@@ -1,13 +1,7 @@
 import json
 from django.http import Http404, HttpResponse
 
-def more_todo(request):
-    if request.is_ajax():
-        todo_items = ['Mow Lawn', 'Buy Groceries',]
-        data = json.dumps(todo_items)
-        return HttpResponse(data, content_type='application/json')
-    else:
-        raise Http404
+
 
 def add_todo(request):
     if request.is_ajax() and request.POST:
